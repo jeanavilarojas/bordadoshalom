b1e483ba368cfe3e7e075a422a3ba8ae69b594f6
Jean Franco <74008542+jeanavilarojas@users.noreply.github.com>
2022-04-26 15:58:30 -0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Volver a Inicio
+function scrollTopBack() {
+    let scrollTopButton = document.querySelector("#scrollUp");
+    window.onscroll = function () {
+        var scroll = document.documentElement.scrollTop;
+        if (scroll >= 250) {
+            scrollTopButton.classList.add('scrollActive');
+        } else {
+            scrollTopButton.classList.remove('scrollActive');
+        }
+    }
+}
+scrollTopBack();
